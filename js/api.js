3c0ea9cc4e53594dd8b0a191955312bb9c97468c
quynhha <67792778+quynhha@users.noreply.github.com>
2021-11-28 19:47:45 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,6 +1,6 @@
 // all access driven through BASE. Must end with a SLASH
 // be sure you change to accommodate your specific API Gateway entry point
-var base_url = "";
+var base_url = "https://x6uhf9obg5.execute-api.us-east-2.amazonaws.com/beta";
 
 var list_url   = base_url + "projects";    // GET
 var create_url = base_url + "projects";    // POST
