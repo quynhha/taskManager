9aa0a9d28ae4a19fa9246e4fabb50d4665cd882b
rnandigani <59891893+rnandigani@users.noreply.github.com>
2021-11-28 21:20:08 -0500

    getAllProjects

--- src/main/java/com/amazonaws/lambda/demo/db/ProjectsDAO.java
+++ src/main/java/com/amazonaws/lambda/demo/db/ProjectsDAO.java
@@ -1,8 +1,13 @@
 package com.amazonaws.lambda.demo.db;
 
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.amazonaws.lambda.demo.model.Project;
+
+import edu.wpi.cs.heineman.demo.model.Constant;
 
 public class ProjectsDAO {
 
@@ -16,9 +21,32 @@ public class ProjectsDAO {
 		return false;
 	}
 
-	public List<Project> getAllProjects() {
+	public List<Project> getAllProjects() throws Exception {
 		// TODO Auto-generated method stub
-		return null;
-	}
+		//return null;
+		
+		List<Project> allProjects = new ArrayList<>();
+        try {
+            Statement statement = conn.createStatement();
+            String query = "SELECT * FROM " + tblName + ";";
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                Project p = generateProject(resultSet);
+                allProjects.add(p);
+            }
+            resultSet.close();
+            statement.close();
+            return allProjects;
+
+        } catch (Exception e) {
+            throw new Exception("Failed in getting projects: " + e.getMessage());
+        }
+    }
+    
+    private Project generateProject(ResultSet resultSet) throws Exception {
+        String id  = resultSet.getString("id");
+        return new Project (id);
+    }
 
 }
