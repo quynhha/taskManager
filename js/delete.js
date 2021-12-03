c10a3036aec483ec2b8a1d188bf6d16114b05087
quynhha <67792778+quynhha@users.noreply.github.com>
2021-12-02 22:04:13 -0500

    html

--- js/delete.js
+++ js/delete.js
@@ -6,11 +6,21 @@ function processDeleteResponse(result){
 }
 
 function requestDelete(val){
+    var form = document.deleteProject;
+
+    var data = {}; 
+    data["name"] = form.projectName.value; 
+    console.log(form.projectName.value);
+
+    val = form.projectName.value;
+
     if(confirm("Process to delete this project: " + val)){
         processDelete(val);
     }
 }
 function processDelete(name){
+    var announ = document.getElementById('project-anno');
+    var output ="";
     var xhr = new XMLHttpRequest();
     xhr.open("POST", delete_url + "/" + name, true); 
 
@@ -22,12 +32,14 @@ function processDelete(name){
             if(xhr.status == 200){
                 console.log("actual:" + xhr.responseText);
                 processDeleteResponse(xhr.responseText);
+                output = "Project Deleted!"
             }
             else{
                 console.log("Actual:" + xhr.responseText); 
                 var js = JSON.parse(xhr.responseText); 
                 var err = js["error"];
                 alert(err);
+                output = "Something is wrong!"
             }
         }
         else{
@@ -35,4 +47,6 @@ function processDelete(name){
         }
     };
     xhr.send(null);
+    announ.innerHTML = output;
+
 }
