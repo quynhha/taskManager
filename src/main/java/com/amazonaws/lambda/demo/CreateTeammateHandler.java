6838640db709fae3ac790930ad41bb17a79fe45f
rnandigani <59891893+rnandigani@users.noreply.github.com>
2021-12-04 18:21:17 -0500

    Teammate initial lambda function

--- src/main/java/com/amazonaws/lambda/demo/CreateTeammateHandler.java
+++ src/main/java/com/amazonaws/lambda/demo/CreateTeammateHandler.java
@@ -1,43 +1,22 @@
 package com.amazonaws.lambda.demo;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
-
-import com.amazonaws.lambda.demo.db.ProjectsDAO;
-import com.amazonaws.lambda.demo.db.TaskDAO;
 import com.amazonaws.lambda.demo.db.TeammateDAO;
 import com.amazonaws.lambda.demo.http.GetTeammateRequest;
 import com.amazonaws.lambda.demo.http.GetTeammateResponse;
-import com.amazonaws.lambda.demo.model.Project;
-import com.amazonaws.lambda.demo.model.Task;
 import com.amazonaws.lambda.demo.model.Teammate;
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.lambda.runtime.events.S3Event;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.GetObjectRequest;
-import com.amazonaws.services.s3.model.S3Object;
 
 
 public class CreateTeammateHandler implements RequestHandler<GetTeammateRequest, GetTeammateResponse> {
-	
-	LambdaLogger logger;
-	
-	// To access S3 storage
-	@SuppressWarnings("unused")
-	private AmazonS3 s3 = AmazonS3ClientBuilder.standard().build();
-	
-	public static final String REAL_BUCKET = "teammate/";
 	
 	/** Store into RDS.
 	 * 
 	 * @throws Exception 
 	 */
 	boolean createTeammate(String name) throws Exception { 
-		if (logger != null) { logger.log("in createTeammate"); }
+
 		TeammateDAO dao = new TeammateDAO();
 		
 		// check if present
@@ -52,8 +31,6 @@ public class CreateTeammateHandler implements RequestHandler<GetTeammateRequest,
 	
 	@Override 
 	public GetTeammateResponse handleRequest(GetTeammateRequest req, Context context)  {
-		logger = context.getLogger();
-		logger.log(req.toString());
 
 		GetTeammateResponse response;
 		try {
@@ -75,30 +52,6 @@ public class CreateTeammateHandler implements RequestHandler<GetTeammateRequest,
 
     public CreateTeammateHandler() {}
 
-    // Test purpose only.
-    CreateTeammateHandler(AmazonS3 s3) {
-        this.s3 = s3;
-    }
-
-    public String handleRequest(S3Event event, Context context) {
-        context.getLogger().log("Received event: " + event);
-
-        // Get the object from the event and show its content type
-        String bucket = event.getRecords().get(0).getS3().getBucket().getName();
-        String key = event.getRecords().get(0).getS3().getObject().getKey();
-        try {
-            S3Object response = s3.getObject(new GetObjectRequest(bucket, key));
-            String contentType = response.getObjectMetadata().getContentType();
-            context.getLogger().log("CONTENT TYPE: " + contentType);
-            return contentType;
-        } catch (Exception e) {
-            e.printStackTrace();
-            context.getLogger().log(String.format(
-                "Error getting object %s from bucket %s. Make sure they exist and"
-                + " your bucket is in the same region as this function.", key, bucket));
-            throw e;
-        }
-    }
     
     @Test //if teammateName already exists within PROJECT
 	public void testTeammateExists() {
