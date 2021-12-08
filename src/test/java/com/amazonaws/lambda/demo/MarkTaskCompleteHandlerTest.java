389797caad0b04a9665e27a30fc1fb26f110cb05
samnate17 <70825860+samnate17@users.noreply.github.com>
2021-12-08 15:50:57 -0500

    MarkTestComplete handler test

---
+++ src/test/java/com/amazonaws/lambda/demo/MarkTaskCompleteHandlerTest.java
@@ -0,0 +1,38 @@
+package com.amazonaws.lambda.demo;
+
+
+import java.io.IOException;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.amazonaws.lambda.demo.http.ArchiveProjectRequest;
+import com.amazonaws.lambda.demo.http.ArchiveProjectResponse;
+import com.amazonaws.lambda.demo.http.GetProjectRequest;
+import com.amazonaws.lambda.demo.http.GetProjectResponse;
+import com.amazonaws.lambda.demo.http.MarkTaskCompleteRequest;
+import com.amazonaws.lambda.demo.http.MarkTaskCompleteResponse;
+import com.amazonaws.lambda.demo.model.Project;
+
+/**
+ * A simple test harness for locally invoking your Lambda function handler.
+ */
+public class MarkTaskCompleteHandlerTest extends LambdaTest {
+	 	
+	@Test
+	public void testMarkTaskComplete() throws IOException {
+		  	MarkTaskCompleteHandler handler = new MarkTaskCompleteHandler();
+
+		 	String var = "A";
+		 	String var2 = "0";
+		 	MarkTaskCompleteRequest req = new MarkTaskCompleteRequest(var, var2);
+		 	
+		 	
+		 	MarkTaskCompleteResponse resp = handler.handleRequest(req, createContext("list"));
+	    	
+	       
+	        Assert.assertEquals(200, resp.statusCode);
+	    }
+
+    
+}
