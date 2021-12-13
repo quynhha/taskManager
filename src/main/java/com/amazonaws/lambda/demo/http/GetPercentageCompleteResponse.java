c82b8b31a621de783d0ca6b02d208fc771b25136
samnate17 <70825860+samnate17@users.noreply.github.com>
2021-12-13 15:13:21 -0500

    update GetPercentageCompleteResponse

--- src/main/java/com/amazonaws/lambda/demo/http/GetPercentageCompleteResponse.java
+++ src/main/java/com/amazonaws/lambda/demo/http/GetPercentageCompleteResponse.java
@@ -5,20 +5,23 @@ package com.amazonaws.lambda.demo.http;
 public class GetPercentageCompleteResponse {
 		public final int statusCode;
 		public final String error;
+		public int percentageComplete;
 
 		public GetPercentageCompleteResponse (int code) {
 			this.statusCode = code;
 			this.error = "";
 		}
 		
-		public GetPercentageCompleteResponse (int code, String errorMessage) {
+		public GetPercentageCompleteResponse (int code, String errorMessage , int percentageComplete) {
 			this.statusCode = code;
 			this.error = errorMessage;
+			this.percentageComplete = percentageComplete;
 		}
 		
 		public String toString() {
 			
-			return "PercentageComplete" + statusCode;
+			return "PercentageComplete" + statusCode + percentageComplete;
+			
 		}
 
 }
